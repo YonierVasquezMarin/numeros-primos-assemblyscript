99d69868f9719a13938e182ddbdde68b13e921ff
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-04-23 21:32:37 -0500

    Cambio de configuracion

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,23 +1,14 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
+export function listaPrimos(cantidad: i32): Array<i64> {
 
-export function primos(cantidad: i32): Array<i32> {
-  console.log("Iniciando la función");
-
-  let numerosPrimos: Array<i32> = [];
+  let numerosPrimos: Array<i64> = [];
   let cursor: i32 = 1;
 
   while (numerosPrimos.length != cantidad) {
-    console.log("Iniciando el while");
-
-    let cantidadDivisores: Array<i32> = [];
+    let cantidadDivisores: Array<u32> = [];
 
     for (let i = 1; i < cursor + 1; i++) {
-      console.log("iniciando el for");
-
       let division: f32 = f32(cursor) / f32(i);
       let esLaDivisionEntera: boolean = division % 1 == 0;
 
@@ -33,7 +24,6 @@ export function primos(cantidad: i32): Array<i32> {
 
     if (cantidadDivisores.length == 2) {
       numerosPrimos.push(cursor);
-      console.log("Se agrega un primo");
     }
 
     // Resetear estas variables para el próximo número
